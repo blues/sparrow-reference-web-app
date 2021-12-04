bd7551818f9e4026752bcabcbba131b45c967b52
Rob Gibbons <robwgibbons@gmail.com>
2021-12-03 19:46:29 -0500

    feat: Add more HTTP methods

--- pages/api/gateway/[gatewayUID]/environment-vars.ts
+++ pages/api/gateway/[gatewayUID]/environment-vars.ts
@@ -5,16 +5,46 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({
-    "environment_variables": {
-      "_tags": "rocks",
-      "sensordb_reset_counts": "17"
-    },
-    "environment_variables_env_default": {
-      "env_update_mins": "5",
-      "pairing_timeout_mins": "60",
-      "sensordb_reset_counts": "0",
-      "sensordb_update_mins": "60"
-    }
-  });
+  switch (req.method) {
+    case 'PUT':
+      // Stubbed response for SET Environment Variable API call
+      res.status(200).json({
+        "environment_variables": {
+          "_tags": "",
+          "device_humidity": "55.9",
+          "device_temp": "79.0"
+        }
+      });
+      break;
+    case 'DELETE':
+      // Stubbed response for DELETE Environment Variable API call
+      res.status(200).json({
+        "environment_variables": {
+          "_tags": "",
+          "device_humidity": "55.9",
+          "device_temp": "79.0"
+        }
+      });
+      break;
+    case 'GET':
+      // Stubbed response for GET Environment Variable API call
+      res.status(200).json({
+        "environment_variables": {
+          "_tags": "",
+          "device_humidity": "55.9",
+          "device_temp": "79.0"
+        },
+        "environment_variables_env_default": {
+          "env_update_mins": "5",
+          "pairing_timeout_mins": "60",
+          "sensordb_reset_counts": "0",
+          "sensordb_update_mins": "60"
+        }
+      });
+      break;
+    default:
+      // Other methods not allowed at this route
+      res.status(405).end();
+      break;
+  }
 }
--- pages/api/gateway/[gatewayUID]/sensor/[macAddress]/config.ts
+++ pages/api/gateway/[gatewayUID]/sensor/[macAddress]/config.ts
@@ -5,12 +5,25 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({
-    "note": "20323746323650020031002f",
-    "body": {
-      "loc": "87JFH688+2H",
-      "name": "0F Furnace"
-    },
-    "time": 1632763519
-  });
+  switch (req.method) {
+    case 'POST':
+      // Stubbed response for note.update success (empty JSON object)
+      res.status(200).json({});
+      break;
+    case 'GET':
+      // Stubbed response for note.get for sensor config.db
+      res.status(200).json({
+        "note": "20323746323650020031002f",
+        "body": {
+          "loc": "87JFH688+2H",
+          "name": "0F Furnace"
+        },
+        "time": 1632763519
+      });
+      break;
+    default:
+      // Other methods not allowed at this route
+      res.status(405).end();
+      break;
+  }
 }
