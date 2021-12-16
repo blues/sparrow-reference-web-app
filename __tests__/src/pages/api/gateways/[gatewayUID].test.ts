bda7eda5d1c09f1770a9e1dc13cd0ab9862bb9e6
Paige Niedringhaus <paigen11@gmail.com>
2021-12-16 10:55:30 -0500

    cleaning up eslint and ts errors

--- __tests__/src/pages/api/gateways/[gatewayUID].test.ts
+++ __tests__/src/pages/api/gateways/[gatewayUID].test.ts
@@ -1,7 +1,8 @@
 /**
  * @jest-environment node
  */
-import { createMocks, RequestOptions, ResponseOptions } from "node-mocks-http";
+import { createMocks } from "node-mocks-http";
+import type { NextApiRequest, NextApiResponse } from "next";
 import gatewaysHandler from "../../../../../src/pages/api/gateways/[gatewayUID]";
 
 describe("/api/gateways/[gatewayUID] API Endpoint", () => {
@@ -10,56 +11,60 @@ describe("/api/gateways/[gatewayUID] API Endpoint", () => {
   const authToken = process.env.HUB_AUTH_TOKEN;
   const gatewayUID = process.env.HUB_DEVICE_UID;
 
-  const { req, res }: { req: RequestOptions; res: ResponseOptions } =
-    createMocks({ method: "GET" });
+  const { req, res }: { req: NextApiRequest; res: NextApiResponse } =
+    createMocks({
+      method: "GET",
+    });
 
   beforeEach(() => {
     req.headers = {
       "Content-Type": "application/json",
       "X-SESSION-TOKEN": authToken,
     };
-    req.query = { gatewayUID };
+    req.query = { gatewayUID: `${gatewayUID}` };
   });
 
   it("should return a successful response from Notehub", async () => {
     await gatewaysHandler(req, res);
 
-    expect(res._getStatusCode()).toBe(200);
-    expect(res._getHeaders()).toEqual({ "content-type": "application/json" });
+    expect(res.statusCode).toBe(200);
+    expect(res.getHeaders()).toEqual({ "content-type": "application/json" });
     expect(res.statusMessage).toEqual("OK");
   });
 
   it("should return a 404 if Gateway UID is invalid", async () => {
     req.query = { gatewayUID: "hello_world" };
 
-    const { res }: { res: ResponseOptions } = createMocks();
+    const { res }: { res: NextApiResponse } = createMocks();
 
     await gatewaysHandler(req, res);
 
-    expect(res._getStatusCode()).toBe(404);
+    expect(res.statusCode).toBe(404);
+    // eslint-disable-next-line no-underscore-dangle
     expect(res._getJSONData()).toEqual({ err: "Unable to find device" });
   });
 
   it("should return a 400 if Gateway UID is missing", async () => {
     req.query = { gatewayUID: null };
 
-    const { res }: { res: ResponseOptions } = createMocks();
+    const { res }: { res: NextApiResponse } = createMocks();
 
     await gatewaysHandler(req, res);
 
-    expect(res._getStatusCode()).toBe(400);
+    expect(res.statusCode).toBe(400);
+    // eslint-disable-next-line no-underscore-dangle
     expect(res._getJSONData()).toEqual({
       err: "Invalid gateway UID parameter",
     });
   });
 
   it("should return a 405 if HTTP method is not GET", async () => {
-    const { req, res }: { req: RequestOptions; res: ResponseOptions } =
+    const { req, res }: { req: NextApiRequest; res: NextApiResponse } =
       createMocks({ method: "POST" });
 
     await gatewaysHandler(req, res);
-
-    expect(res._getStatusCode()).toBe(405);
+    expect(res.statusCode).toBe(405);
+    // eslint-disable-next-line no-underscore-dangle
     expect(res._getJSONData()).toEqual({
       err: "Method not allowed",
     });
