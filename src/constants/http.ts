f3ab4bea2a9ea9f163810f713ca940bd95ec0ef1
Rob Gibbons <robwgibbons@gmail.com>
2021-12-22 12:41:52 -0500

    feat: Add Sensor Config API endpoint

--- src/constants/http.ts
+++ src/constants/http.ts
@@ -2,10 +2,14 @@
 const HTTP_STATUS = {
   METHOD_NOT_ALLOWED: "HTTP method not allowed",
   INVALID_GATEWAY: "Invalid Gateway UID",
+  INVALID_SENSOR_MAC: "Invalid Sensor MAC",
+  INVALID_CONFIG_BODY: "Invalid Sensor Config Body",
   INTERNAL_ERR_GATEWAY: "Error while fetching Gateway",
   INTERNAL_ERR_SENSORS: "Error while fetching Sensors",
+  INTERNAL_ERR_CONFIG: "Error while fetching Sensor Config",
   NOT_FOUND_GATEWAY: "Unable to locate Gateway",
   NOT_FOUND_SENSORS: "Unable to locate Sensors",
+  NOT_FOUND_CONFIG: "Unable to locate Sensor config",
 };
 
 // HTTP headers
