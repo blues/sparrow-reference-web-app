c82a38682f1c67244d65a7daff2fb43b26c691ff
Paige Niedringhaus <paigen11@gmail.com>
2022-01-26 10:25:10 -0500

    Fix sparrow in prod (#33)
    * upgrading netlify plugin to fix the build
    
    * trying a fix by making app base url dynamic
    
    * let DEPLOY_URL override APP_BASE_URL on Netlify
    
    * fix ts
    
    * tj's servicelocator fix for build-time
    
    * fixing broken unit tests to get this fix in to main
    
    * changing one val at a time to debug
    
    * trying something else
    
    * try 3
    
    * try 4
    
    * pls work
    
    * pls work
    
    * maybe its the start date
    
    * a missing return statement?!?!
    
    * moving return
    
    Co-authored-by: Carlton Henderson <henderson.carlton@gmail.com>

--- src/services/notehub/AxiosHttpNotehubAccessor.ts
+++ src/services/notehub/AxiosHttpNotehubAccessor.ts
@@ -1,21 +1,33 @@
 import axios from "axios";
 import { NotehubAccessor } from "./NotehubAccessor";
 import NotehubDevice from "./models/NotehubDevice";
-import Config from "../../../config";
+import { HTTP_HEADER } from "../../constants/http";
 
 // this class directly interacts with Notehub via HTTP calls
 export default class AxiosHttpNotehubAccessor implements NotehubAccessor {
-  // todo eventually our config will be supplied from one source of truth in the app
-  appBaseUrl: string;
+  baseURL: string;
 
-  constructor() {
-    this.appBaseUrl = Config.appBaseUrl;
+  hubDeviceUID: string;
+
+  commonHeaders;
+
+  constructor(
+    hubBaseURL: string,
+    hubAppUID: string,
+    hubDeviceUID: string,
+    hubAuthToken: string
+  ) {
+    this.baseURL = `${hubBaseURL}/v1/projects/${hubAppUID}`;
+    this.hubDeviceUID = hubDeviceUID;
+    this.commonHeaders = {
+      [HTTP_HEADER.CONTENT_TYPE]: HTTP_HEADER.CONTENT_TYPE_JSON,
+      [HTTP_HEADER.SESSION_TOKEN]: hubAuthToken,
+    };
   }
 
   async getGateway(hubDeviceUID: string) {
-    const resp = await axios.get(
-      `${this.appBaseUrl}/api/gateways/${hubDeviceUID}`
-    );
+    const endpoint = `${this.baseURL}/devices/${hubDeviceUID}`;
+    const resp = await axios.get(endpoint, { headers: this.commonHeaders });
     return resp.data as NotehubDevice;
   }
 }
