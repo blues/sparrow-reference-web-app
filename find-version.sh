b243e8285b83bf9999b6c8279f209e44d78975a4
Carlton Henderson <henderson.carlton@gmail.com>
2022-01-31 11:20:46 -0500

    better form

--- find-version.sh
+++ find-version.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-echo "$(date --iso-8601 --utc)-$(git describe --exclude='*' --always --dirty=-delta)-#$GITHUB_RUN_NUMBER-$GITHUB_RUN_ATTEMPT"
+echo \
+    "$(date --iso-8601 --utc)" \
+    "$(git describe --exclude='*' --always --dirty=-delta)" \
+    "$NETLIFY $BUILD_ID $CONTEXT" \
 
