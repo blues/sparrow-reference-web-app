6b67b60bc0d2feaf76c03de4acf299d158df5d36
Carlton Henderson <henderson.carlton@gmail.com>
2022-01-31 11:25:07 -0500

    some builds are in github so don't leave that out

--- find-version.sh
+++ find-version.sh
@@ -2,5 +2,6 @@
 echo \
     "$(date --rfc-3339=seconds)" \
     "$(git describe --exclude='*' --always --dirty=-delta)" \
-    "$NETLIFY $BUILD_ID $CONTEXT" \
+    "GH#$GITHUB_RUN_NUMBER-$GITHUB_RUN_ATTEMPT" \
+    "N$BUILD_ID $CONTEXT" \
 
