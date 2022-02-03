095ccbf79ff4ce7fb79164b80dbd38ef2f65715f
Paige Niedringhaus <paigen11@gmail.com>
2022-02-03 12:11:16 -0500

    adding new app service unit tests

--- __tests__/src/services/AppService.test.ts
+++ __tests__/src/services/AppService.test.ts
@@ -6,6 +6,10 @@ describe("App Service", () => {
   let dataProviderMock: DataProvider;
   let appServiceMock: AppService;
 
+  const { mockedGatewayUID } = sparrowData;
+
+  const { mockedSensorUID } = sparrowData;
+
   const mockedGatewaySparrowData =
     sparrowData.successfulGatewaySparrowDataResponse;
 
@@ -13,25 +17,39 @@ describe("App Service", () => {
     sparrowData.successfulGatewaySparrowDataResponse,
   ];
 
+  const mockedSensorSparrowData =
+    sparrowData.successfulSensorSparrowDataResponse;
+
+  const mockedSensorsSparrowData = [
+    sparrowData.successfulSensorSparrowDataResponse,
+  ];
+
   beforeEach(() => {
     dataProviderMock = {
       getGateway: jest.fn().mockResolvedValueOnce(mockedGatewaySparrowData),
       getGateways: jest.fn().mockResolvedValueOnce(mockedGatewaysSparrowData),
-      // placeholder mocked function to keep TS from yelling
-      getLatestSensorData: jest.fn(),
+      getSensor: jest.fn().mockResolvedValueOnce(mockedSensorSparrowData),
+      getSensors: jest.fn(),
+      getSensorData: jest.fn(),
     };
     appServiceMock = new AppService(dataProviderMock);
   });
 
   it("should return a single gateway when getGateway is called", async () => {
-    const mockGatewayUID = "dev:5678";
-
-    const res = await appServiceMock.getGateway(mockGatewayUID);
+    const res = await appServiceMock.getGateway(mockedGatewayUID);
     expect(res).toEqual(mockedGatewaySparrowData);
   });
 
   it("should return a list of gateways when getGateways is called", async () => {
     const res = await appServiceMock.getGateways();
     expect(res).toEqual(mockedGatewaysSparrowData);
+  });
+
+  it("should return a single sensor when getSensor is called", async () => {
+    const res = await appServiceMock.getSensor(
+      mockedGatewayUID,
+      mockedSensorUID
+    );
+    expect(res).toEqual(mockedSensorSparrowData);
   });
 });
