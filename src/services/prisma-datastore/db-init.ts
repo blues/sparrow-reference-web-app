374a3cce825ccdbad97ff058815ec6df84e1bf67
Matthew McGowan <mat.mcgowan+git@gmail.com>
2022-02-17 22:05:30 -0800

    start of database initialization script. getting ts to run standalone was tricky.

---
+++ src/services/prisma-datastore/db-init.ts
@@ -0,0 +1,54 @@
+import { PrismaClient, Project, Gateway, DeviceReadingSchema, Reading, Prisma } from '@prisma/client'
+import { loadEnvConfig } from '@next/env'
+import Config from '../../../config'
+
+async function loadEnvironment() {
+   const projectDir = process.cwd()
+   console.log(projectDir)
+   await loadEnvConfig(projectDir)
+}
+
+const prisma = new PrismaClient()
+
+
+async function upsertProject() {
+
+    const upsert = {
+        where: {
+            projectUID:  Config.hubProjectUID
+        } ,  // looks like the typescript info is broken and only supports id
+        create: {
+            projectUID:  Config.hubProjectUID,
+            name: Config.companyName,
+        },
+        update: {
+            name: Config.companyName
+        }
+    } as Prisma.ProjectUpsertArgs;
+    await prisma.project.upsert(upsert);
+}
+
+async function createProject() {
+
+    console.log("Creating project", Config.hubProjectUID)
+    await prisma.project.create({
+        data: {
+            projectUID:  Config.hubProjectUID,
+            name: Config.companyName,
+        }
+    })
+}
+
+async function init() {
+// next.js loadEnvironment replaces process.env with a custom object. We don't want that. 
+// so just be sure the environment is setup normally. will later use dotenv 
+    //    await loadEnvironment();
+    await createProject();
+}
+
+init().catch((e) => {
+  throw e
+})
+.finally(async () => {
+  await prisma.$disconnect()
+})
