d43ebb13f6ec8444a3be23cb94016255b63bb762
Carlton Henderson <henderson.carlton@gmail.com>
2022-03-02 14:13:44 -0500

    fix: to allow naming a node that has no name

--- src/services/notehub/NotehubAttributeStore.ts
+++ src/services/notehub/NotehubAttributeStore.ts
@@ -1,13 +1,20 @@
 import { AttributeStore } from "../AttributeStore";
+import NoteSensorConfigBody from "./models/NoteSensorConfigBody";
 import { NotehubAccessor } from "./NotehubAccessor";
 
 export class NotehubAttributeStore implements AttributeStore {
   constructor(private accessor: NotehubAccessor) {}
 
+  async getNodeConfig(gatewayUID: string, nodeID: string) {
+    const defaultConfig = {} as NoteSensorConfigBody;
+    const { body } = await this.accessor.getConfig(gatewayUID, nodeID);
+    return body || defaultConfig;
+  }
+
   async updateSensorName(gatewayUID: string, macAddress: string, name: string) {
-    const { body } = await this.accessor.getConfig(gatewayUID, macAddress);
-    body.name = name;
-    await this.accessor.setConfig(gatewayUID, macAddress, body);
+    const config = await this.getNodeConfig(gatewayUID, macAddress);
+    config.name = name;
+    await this.accessor.setConfig(gatewayUID, macAddress, config);
   }
 
   async updateSensorLocation(
@@ -15,9 +22,9 @@ export class NotehubAttributeStore implements AttributeStore {
     macAddress: string,
     loc: string
   ) {
-    const { body } = await this.accessor.getConfig(gatewayUID, macAddress);
-    body.loc = loc;
-    await this.accessor.setConfig(gatewayUID, macAddress, body);
+    const config = await this.getNodeConfig(gatewayUID, macAddress);
+    config.loc = loc;
+    await this.accessor.setConfig(gatewayUID, macAddress, config);
   }
 }
 
