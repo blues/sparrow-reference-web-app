d108ebade42cfb9c84dc6eb2f961787fe6ce4242
Matthew McGowan <mat.mcgowan+git@gmail.com>
2022-03-23 14:13:22 -0700

    revert changes to next.js serialization

--- next.config.js
+++ next.config.js
@@ -9,19 +9,3 @@ module.exports = withAntdLess({
   // optional https://github.com/webpack-contrib/css-loader#object
   cssLoaderOptions: {},
 });
-
-
-/**
- * Remove undefined values so Next.js doesn't complain during serialization. Verified as of v11.1.2.
- */
- const removeUndefined = (obj) => {
-  let newObj = {};
-  Object.keys(obj).forEach((key) => {
-    if (obj[key] === Object(obj[key])) newObj[key] = removeUndefined(obj[key]);
-    else if (obj[key] !== undefined) newObj[key] = obj[key];
-  });
-  return newObj;
-};
-const next = require('next/dist/lib/is-serializable-props');
-const isSerializableProps = next.isSerializableProps;
-next.isSerializableProps = (page, method, input) => isSerializableProps(page, method, removeUndefined(input));
