979c0bcb74c381b5e0b707504123dd81714fdb7b
Paige Niedringhaus <paigen11@gmail.com>
2022-04-01 15:25:35 -0400

    poc of how we could style the new ant d menu to closely resemble notehub menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { Menu } from "antd";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 
@@ -11,11 +17,17 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isLoading, setIsLoading] = useState(false);
   const Router = useRouter();
 
+  const { SubMenu } = Menu;
+
   useEffect(() => {
     Router.events.on("routeChangeStart", () => setIsLoading(true));
     Router.events.on("routeChangeComplete", () => setIsLoading(false));
     Router.events.on("routeChangeError", () => setIsLoading(false));
   }, [Router.events]);
+
+  const handleClick = (e) => {
+    console.log("click ", e);
+  };
 
   return (
     <>
@@ -35,6 +47,41 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           href="/favicon-16x16.png"
         />
       </Head>
+      <Menu
+        onClick={handleClick}
+        style={{ width: 256 }}
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub1"]}
+        mode="inline"
+      >
+        <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+          <Menu.ItemGroup key="g1" title="Item 1">
+            <Menu.Item key="g1">Option 1</Menu.Item>
+            <Menu.Item key="2">Option 2</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup key="g2" title="Item 2">
+            <Menu.Item key="g2">Option 3</Menu.Item>
+            <Menu.Item key="4">Option 4</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+        <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
+          <Menu.Item key="5">Option 5</Menu.Item>
+          <Menu.Item key="6">Option 6</Menu.Item>
+          <SubMenu key="sub3" title="Submenu">
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item>
+          </SubMenu>
+        </SubMenu>
+        <SubMenu key="sub4" icon={<SettingOutlined />} title="Navigation Three">
+          <Menu.Item key="9">Option 9</Menu.Item>
+          <Menu.Item key="10">Option 10</Menu.Item>
+          <Menu.Item key="11">Option 11</Menu.Item>
+          <Menu.Item key="12">Option 12</Menu.Item>
+        </SubMenu>
+        <Menu.Item className="top-menu" key="13">
+          Option 13
+        </Menu.Item>
+      </Menu>
       <Layout isLoading={isLoading}>
         <Component {...pageProps} />
       </Layout>
