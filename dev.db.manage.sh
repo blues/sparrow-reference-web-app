3cab42d3fcb604c5c3add6c59c5f5213b89932c3
Carlton Henderson <chenderson@blues.com>
2022-04-27 00:04:24 +0000

    mor windows changes

--- dev.db.manage.sh
+++ dev.db.manage.sh
@@ -3,6 +3,14 @@
 # Bash strict
 set -euo pipefail
 
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$SCRIPT_DIR" # cd to this script's dir
+
+set -o allexport
+source .env
+set +o allexport
+
 #### Manage the database with a webapp
 yarn prisma studio
 
