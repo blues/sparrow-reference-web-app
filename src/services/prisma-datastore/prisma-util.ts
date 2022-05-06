33e15a09b92e69fb12ba018d8a8a07f1e0fa8c00
TJ VanToll <tj.vantoll@gmail.com>
2022-05-06 10:58:19 -0400

    Make the TS compiler happy

--- src/services/prisma-datastore/prisma-util.ts
+++ src/services/prisma-datastore/prisma-util.ts
@@ -1,9 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import { PrismaClient } from "@prisma/client";
 
-// From https://github.com/vercel/next.js/issues/7811#issuecomment-618425485
+declare global {
+  // eslint-disable-next-line no-var, vars-on-top
+  var prisma: PrismaClient;
+}
+
 // Prevent Next.js from using creating multiple Prisma connections
 // during development.
+// See: https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
 export function getPrismaClient(databaseURL: string): PrismaClient {
   if (!global.prisma) {
     global.prisma = new PrismaClient({
